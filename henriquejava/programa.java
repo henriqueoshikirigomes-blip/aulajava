860af5bcfb06a8f9aa11e1671d739c84d5d67003
henriqueoshikirigomes-blip <henrique.oshikiri.gomes@escola.pr.gov.br>
2026-05-22 11:19:17 -0300

    Add files via upload

---
+++ henriquejava/programa.java
@@ -0,0 +1,10 @@
+public class programa {
+    public static void main(String[] args) {
+        String nome = "henrique";
+        int idade = 17 ;
+        double altura = 1.78;
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Altura: " + altura);
+    }
+}
